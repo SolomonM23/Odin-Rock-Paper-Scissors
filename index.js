eed21146924755cd90a4aa4eb20ee3bdb218cc17
Solomon <kingsolomontm@gmail.com>
2023-12-10 16:33:58 -0500

    first implementation of game()

--- index.js
+++ index.js
@@ -51,5 +51,38 @@ function playRound(playerSelection, computerSelection){
     } 
 }
 
-
 playRound(playerSelection, computerSelection);
+
+function game(){
+    let playerScore = 0;
+    let compScore = 0;
+
+    //replay round if a tie
+    if (playRound() == 'its a tie'){
+        playRound(playerSelection, computerSelection);
+    }
+
+    //player point
+    if (playRound().slice(0, 7) == 'You win!'){
+        playerScore += 1;
+        console.log(playerScore);
+    }
+
+    //computer point
+    if (playRound().slice(0, 8) == 'You lose!'){
+        compScore += 1;
+        console.log(compScore);
+    }
+
+    //counter for player score
+    for (playerScore; playerScore >= 3; playerScore++){
+        console.log('Your Score: ' + (playerScore += 1));
+    }
+
+    //counter for computer score
+    for (compScore; compScore >= 3; compScore++){
+        console.log('Computer Score: ' + (compScore+= 1));
+    }
+}
+
+game();
